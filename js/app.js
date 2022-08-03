9d9c0d596cdc9110abecca9fc21673c19f33ce2d
LuigiDiS <luigi.distefano69@gmail.com>
2022-08-03 16:52:58 +0200

    Bonus 1

--- js/app.js
+++ js/app.js
@@ -6,41 +6,42 @@ const team = [
     {
         nome: 'Wayne Barnett',
         role: 'Founder & CEO',
-        img: 'wayne-barnett-founder-ceo.jpg',
+        img: './img/wayne-barnett-founder-ceo.jpg',
     },
     {
         nome: 'Angela Caroll',
         role: 'Chief Editor',
-        img: 'angela-caroll-chief-editor.jpg',
+        img: './img/angela-caroll-chief-editor.jpg',
     },
     {
         nome: 'Walter Gordon',
         role: 'Office Manager',
-        img: 'walter-gordon-office-manager.jpg',
+        img: './img/walter-gordon-office-manager.jpg',
     },
     {
         nome: 'Angela Lopez',
         role: 'Social Media Manager',
-        img: 'angela-lopez-social-media-manager.jpg',
+        img: './img/angela-lopez-social-media-manager.jpg',
     },
     {
         nome: 'Scott Estrada',
         role: 'Developer',
-        img: 'scott-estrada-developer.jpg',
+        img: './img/scott-estrada-developer.jpg',
     },
     {
         nome: 'Scott Estrada',
         role: 'Graphic Designer',
-        img: 'barbara-ramos-graphic-designer.jpg',
+        img: './img/barbara-ramos-graphic-designer.jpg',
     },
 ]
 // console.log(team.length);
+
 
 for (let i = 0; i < team.length; i++) {
     const nome = team[i].nome;
     const role = team[i].role;
     const img = team[i].img;
     console.log(`Nome: ${nome}\n` + `Role: ${role}\n` + `url: ${img}`);
-    ulEl.innerHTML += `<li>Nome: ${nome},\n` + `Role: ${role},\n` + `url: ${img}</li>`;
+    ulEl.innerHTML += `<li>Nome: ${nome},\n` + `Role: ${role},\n` + `<img src="${img}" alt="Foto ${nome}"></img></li>`;
 }
 
