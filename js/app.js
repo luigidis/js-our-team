d15f69822e63ef1f05c0c982c76631952a18b866
LuigiDiS <luigi.distefano69@gmail.com>
2022-08-03 16:32:35 +0200

    Milestone 0

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
 console.log('yo');
+
+const team = [
+    {
+        nome: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        role: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        nome: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        role: 'Developer',
+        img: 'scott-estrada-developer.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        role: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg',
+    },
+]
+
